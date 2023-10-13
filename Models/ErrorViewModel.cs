5f87c40aa1da54d1495b01ba1c7d16fabdc6220d
ayoubzann <ayoubzann@gmail.com>
2023-10-13 12:01:20 +0200

    Fixing Git issues

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mvcSaltTraining.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
